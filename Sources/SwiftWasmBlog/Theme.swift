84aed5fedfcc0abcf75807737f621d9074f7d64e
Max Desiatov <max@desiatov.com>
2020-10-29 22:09:15 +0000

    Update fonts

--- Sources/SwiftWasmBlog/Theme.swift
+++ Sources/SwiftWasmBlog/Theme.swift
@@ -232,8 +232,73 @@ private extension HTML {
   ) -> HTML {
     HTML(
       .lang(context.site.language),
-      .head(for: location, on: context.site, stylesheetPaths: ["/styles.css", "/splash.css"]),
+      .customHead(for: location, on: context.site, stylesheetPaths: ["/styles.css", "/splash.css"]),
       .element(named: "body", nodes: body)
+    )
+  }
+}
+
+public extension Node where Context == HTML.DocumentContext {
+  /// Add an HTML `<head>` tag within the current context, based
+  /// on inferred information from the current location and `Website`
+  /// implementation.
+  /// - parameter location: The location to generate a `<head>` tag for.
+  /// - parameter site: The website on which the location is located.
+  /// - parameter titleSeparator: Any string to use to separate the location's
+  ///   title from the name of the website. Default: `" | "`.
+  /// - parameter stylesheetPaths: The paths to any stylesheets to add to
+  ///   the resulting HTML page. Default: `styles.css`.
+  /// - parameter rssFeedPath: The path to any RSS feed to associate with the
+  ///   resulting HTML page. Default: `feed.rss`.
+  /// - parameter rssFeedTitle: An optional title for the page's RSS feed.
+  static func customHead<T: Website>(
+    for location: Location,
+    on site: T,
+    titleSeparator: String = " | ",
+    stylesheetPaths: [Path] = ["/styles.css"],
+    rssFeedPath: Path? = .defaultForRSSFeed,
+    rssFeedTitle: String? = nil
+  ) -> Node {
+    var title = location.title
+
+    if title.isEmpty {
+      title = site.name
+    } else {
+      title.append(titleSeparator + site.name)
+    }
+
+    var description = location.description
+
+    if description.isEmpty {
+      description = site.description
+    }
+
+    return .head(
+      .encoding(.utf8),
+      .siteName(site.name),
+      .url(site.url(for: location)),
+      .title(title),
+      .description(description),
+      .twitterCardType(location.imagePath == nil ? .summary : .summaryLargeImage),
+      .forEach(stylesheetPaths) { .stylesheet($0) },
+      .viewport(.accordingToDevice),
+      .link(
+        .href("https://fonts.googleapis.com/css2?family=Lora:wght@600&display=swap"),
+        .rel(.stylesheet)
+      ),
+      .link(
+        .href("https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"),
+        .rel(.stylesheet)
+      ),
+      .unwrap(site.favicon) { .favicon($0) },
+      .unwrap(rssFeedPath) { path in
+        let title = rssFeedTitle ?? "Subscribe to \(site.name)"
+        return .rssFeedLink(path.absoluteString, title: title)
+      },
+      .unwrap(location.imagePath ?? site.imagePath) { path in
+        let url = site.url(for: path)
+        return .socialImageLink(url)
+      }
     )
   }
 }
